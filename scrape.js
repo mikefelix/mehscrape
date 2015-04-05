48c2225c86e90a3df08cde42b12186c951aa18f4
Michael Felix <michael.felix@gmail.com>
2015-04-05 15:56:43 -0600

    .

--- scrape.js
+++ scrape.js
@@ -1,7 +1,7 @@
 
-var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var exec = require('child_process').exec;
 
 url = 'http://meh.com';
 
@@ -32,29 +32,14 @@ request(url, function(error, response, html) {
             json.images.push($(this).attr('data-src'));
         });
         
-        console.dir(json); 
-
-/*        $('.star-box-giga-star').filter(function() {
-            var data = $(this);
-            rating = data.text();
-
-            json.rating = rating;
-        });*/
+        json.name = json.name.replace(/"/, '\\"');
+        var cmd = 'echo "' + json.name + ' for \\' + json.price + '" | /usr/bin/mail -s "Today\'s Meh deal" michael.felix@gmail.com'
+            console.log(cmd);
+        exec(cmd, function callback(error, stdout, stderr){
+            if (error)
+              console.log("Failed to mail. " + error);
+        });
     }
 	else console.log("Error parsing response.");
-	
-    // To write to the system we will use the built in 'fs' library.
-    // In this example we will pass 3 parameters to the writeFile function
-    // Parameter 1 :  output.json - this is what the created filename will be called
-    // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-    // Parameter 3 :  callback function - a callback function to let us know the status of our function
-
-/*    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
-
-        console.log('File successfully written! - Check your project directory for the output.json file');
-
-    });
-*/
-
 
 });
